5e1dde3057438ccfccf7d2ad95902f68f34ba871
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2024-03-10 22:33:56 +0300

    Update connect.inc.php

--- Updated/inc/connect.inc.php
+++ Updated/inc/connect.inc.php
@@ -1,3 +1,5 @@
 <?php 
-	$con = mysqli_connect("localhost","root","", "grocerydb") or die("Couldn't connect to SQL server");
+	// $con = mysqli_connect("localhost","root","", "grocerydb") or die("Couldn't connect to SQL server");
+	$con = mysqli_connect("sql109.infinityfree.com","if0_36136544","Password1854", "if0_36136544_grocery") or die("Couldn't connect to SQL server");
+
 ?>
